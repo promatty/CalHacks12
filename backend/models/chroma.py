759ab4c6ced136da1c9c632758e1fdfc54e097f7
David He <92886927+ddavidhe@users.noreply.github.com>
2025-10-25 19:08:52 -0700

    integrate chroma db function thing call thing

--- backend/models/chroma.py
+++ backend/models/chroma.py
@@ -2,5 +2,7 @@
 from pydantic import BaseModel
 
 class ChromaRequestModel(BaseModel):
-    query: str
-    context: str
+    currentFile: str
+    fileLength: int
+    distances: list[float]
+    closeFiles: list[str]
--- backend/routers/chroma.py
+++ backend/routers/chroma.py
@@ -3,6 +3,8 @@ from models.chroma import ChromaRequestModel
 from core.config import settings
 from core.models import ChromaModel
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import chromadb
+from chromadb import Search, K, Knn
 
 router = APIRouter(
     prefix="/chroma",
@@ -19,7 +21,7 @@ def verify_api_key(credentials: HTTPAuthorizationCredentials = Security(bearer_s
     if credentials.scheme.lower() != "bearer":
         raise HTTPException(status_code=401, detail="Invalid authentication scheme")
 
-    if credentials.credentials != settings.LAVA_FORWARD_TOKEN:
+    if credentials.credentials != settings.API_KEY:
         raise HTTPException(status_code=401, detail="Invalid API key")
 
     return True
@@ -28,14 +30,37 @@ def verify_api_key(credentials: HTTPAuthorizationCredentials = Security(bearer_s
 async def get_chroma_response(query_request: ChromaRequestModel, authorized: dict = Depends(verify_api_key)):
     #idek what kind of query we want to make here so this is just a placeholder
     chroma_client = ChromaModel.get_chroma_client()
-    response = chroma_client.query(
-        query_texts=[query_request.query],
-        n_results=5,
-        include=["metadatas", "documents"])
+
+    collection = chroma_client.get_collection("promatty_example_calculator_main")
+
+    all_search = (Search()
+               .select(K.DOCUMENT, K.EMBEDDING, K.SCORE, K.METADATA))
+
+    results = collection.search(all_search)
+
+    filenames = list({m["filename"] for m in results["metadatas"][0] if "filename" in m})
+
+    for filename in filenames:
+        search_file = (Search()
+                       .where(K("filename") == filename)
+                       .select(K.ID, K.DOCUMENT, K.METADATA, K.EMBEDDING)
+                        .limit(1)
+                    )
+        results = collection.search(search_file)
+        file_len = len(results['documents'][0][0])
+        embeddings = results['embeddings'][0][0]
+
+        search = Search().select(K.ID, K.SCORE, K.METADATA).limit(4)
+
+        closests = collection.search(search.rank(Knn(query = embeddings)))
+
+        break
 
     return {
-        "status_code": response.status_code,
-        "response": response.json()
+        "currentFile": filename,
+        "fileLength": len(results['documents'][0][0]),
+        "distances": closests['scores'][0][1:4],
+        "closeFiles": [closests['metadatas'][0][i]['filename'] for i in range(1,4)]
     }
 
 
